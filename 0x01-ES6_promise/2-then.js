8a1b7f08707e3efab89edac62746ce5616b078da
Yara-ib <yr.ap.23@gmail.com>
2024-06-27 04:36:45 +0300

    Handling Response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
       resolve({ status: 200, body: 'Success' });
-      console.log('Got a response from the API');
     } else {
       reject(new Error());
-      console.log('Got a response from the API');
     }
-  });
+  }).then(() => console.log('Got a response from the API'));
 }
