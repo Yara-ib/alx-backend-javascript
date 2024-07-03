cedfb08c7d3431061f920e1da4054cfeaea52a2d
Yara-ib <yr.ap.23@gmail.com>
2024-07-04 01:50:25 +0300

    Creating Interface Student

--- 0x04-TypeScript/task_0/dist/js/main.js
+++ 0x04-TypeScript/task_0/dist/js/main.js
@@ -1,14 +1,14 @@
 var studentA = {
-    firstName: 'Jack',
-    lastName: 'Arthur',
-    age: 30,
-    location: 'Location',
+  firstName: 'Jack',
+  lastName: 'Arthur',
+  age: 30,
+  location: 'Location',
 };
 var studentB = {
-    firstName: 'Mary',
-    lastName: 'Corey',
-    age: 30,
-    location: 'Location',
+  firstName: 'Mary',
+  lastName: 'Corey',
+  age: 30,
+  location: 'Location',
 };
 var studentsList = [studentA, studentB];
 //# sourceMappingURL=main.js.map
