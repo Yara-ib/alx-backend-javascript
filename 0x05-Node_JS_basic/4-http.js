7ade6c2069fdbc8bcbe95a20f81c29c4a015e3ac
Yara-ib <yr.ap.23@gmail.com>
2024-09-18 02:04:07 +0300

    Small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,14 @@
 const { createServer } = require('node:http');
-const { error } = require('node:console');
-const { hostname } = require('node:os');
+// const { error } = require('node:console');
+// const { hostname } = require('node:os');
 
 const port = 1245;
+const hostname = '127.0.0.1';
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  if (error) console.error();
-});
+app.listen(port, hostname);
 module.exports = app;
