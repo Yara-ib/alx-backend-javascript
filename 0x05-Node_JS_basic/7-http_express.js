2834515f54bbd2c13959ba8388bf799abe0fad78
Yara-ib <yr.ap.23@gmail.com>
2024-09-19 02:58:21 +0300

    Express with Promise reading a File

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { hostname } = require('os');
+const { error } = require('console');
+const countStudents = require('./7-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(port, hostname, () => {
+  if (error) console.error();
+});
