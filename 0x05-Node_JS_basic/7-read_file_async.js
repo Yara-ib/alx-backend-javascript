f4c10206847e1b2bcfcc5aef80fb60a1a6f363c3
Yara-ib <yr.ap.23@gmail.com>
2024-09-19 02:59:59 +0300

    Express with Promise reading a File

---
+++ 0x05-Node_JS_basic/7-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8')
+    .then((data) => {
+      const cleanedData = data.trim().split('\n').filter((line) => line.length > 0);
+      let studentsNumber = 0;
+      const items = {};
+      /* eslint-disable no-plusplus, no-unused-vars */
+      for (let i = 1; i < cleanedData.length; i++) {
+        const [firstname, lastname, age, field] = cleanedData[i].split(',');
+        if (!items[field]) items[field] = [];
+        items[field].push(firstname);
+        studentsNumber += 1;
+      }
+      /* eslint-disable no-plusplus, no-unused-vars */
+      console.log(`This is the list of our students\nNumber of students: ${studentsNumber}`);
+      for (const item in items) {
+        if (Object.prototype.hasOwnProperty.call(items, item)) {
+          console.log(`Number of students in ${item}: ${items[item].length}. List: ${items[item].join(', ')}`);
+        }
+      }
+      resolve();
+    })
+    .catch(() => {
+      reject(new Error('Cannot load the database'));
+    });
+});
+
+module.exports = countStudents;
