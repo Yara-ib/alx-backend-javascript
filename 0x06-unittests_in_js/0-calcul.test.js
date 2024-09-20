07bcebb38662697fd4edad0e0eff2ab3f3f3cc05
Yara-ib <yr.ap.23@gmail.com>
2024-09-20 22:17:21 +0300

    stricEqual instead of just assert.equal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
-  it('calculateNumber(1, 3)', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+  it('Checking the basic case, no round needed', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('calculateNumber(1, 3.7)', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
+  it('Checking when number needs to be rounded', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('calculateNumber(1.2, 3.7)', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('calculateNumber(1.5, 3.7)', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+  it('Converting string numbers to number & round it', function () {
+    assert.strictEqual(calculateNumber('5.3', 3.7), 9);
   });
 });
