82ff0c8e4f3957c28a841d80517d9e4469644b9d
Yara-ib <yr.ap.23@gmail.com>
2024-09-21 00:06:05 +0300

    Trying adding Error in different way

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,9 +2,8 @@ module.exports = calculateNumber = (type, a, b) => {
   if (type === 'SUM') return Math.round(a) + Math.round(b);
   if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
   if (type === 'DIVIDE') {
-    if (Math.round(b) == 0) throw new Error();
+    if (Math.round(b) == 0) return 'Error';
     return Math.round(a) / Math.round(b);
   } else if (type !== 'SUM' && type !== 'SUBTRACT' && type !== 'DIVIDE')
-    throw new Error();
+    return 'Error';
 };
-
