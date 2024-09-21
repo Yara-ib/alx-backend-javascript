4779d42ac3deb047286a478216263f9ced19f301
Yara-ib <yr.ap.23@gmail.com>
2024-09-22 01:33:05 +0300

    Adding more test cases

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
+const { expect } = require('chai');
 
 describe('Check', function () {
   it('If it called Once or not', function () {
@@ -8,5 +9,32 @@ describe('Check', function () {
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledOnce(stub);
     stub.restore();
+  });
+
+  it('Assume some incorrect value as result, ensuring it using stub', function () {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.withArgs('SUM', 100, 20).returns(500);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 500')).to.be.true;
+    stub.restore();
+    consoleSpy.restore();
+  });
+
+  it('Calling stub twice', function () {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub
+      .withArgs('SUM', 100, 20)
+      .onFirstCall()
+      .returns(500)
+      .onSecondCall()
+      .returns(1000);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 500')).to.be.true;
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 1000')).to.be.true;
+    stub.restore();
+    consoleSpy.restore();
   });
 });
