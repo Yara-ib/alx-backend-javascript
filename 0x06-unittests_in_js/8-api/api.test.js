c43f7ca0dc8837b8595cb9bb882781747a66d5d2
Yara-ib <yr.ap.23@gmail.com>
2024-09-22 03:15:20 +0300

    Basic Test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,14 +4,14 @@ const request = require('request');
 
 describe('Test Suite for Server', function () {
   it('1- Testing result', function (done) {
-    request('http://localhost:7865/', (err, res, body) => {
+    request('http://localhost:7865', (err, res, body) => {
       expect(res.body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
   it('2- Testing status Code', function (done) {
-    request('http://localhost:7865/', (err, res, body) => {
+    request('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       done();
     });
